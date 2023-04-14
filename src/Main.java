3fb770c7fefac4ea67d8cb93c3371f874df45dab
Pablo Fernando Pineda <pinedapablo6718@gmail.com>
2023-04-14 10:01:57 -0500

    Ola

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Hello world!");
+
+
+
     }
+
+    public void pablopineda(){
+        System.out.println("Ay mi madre el bisho, SiuUuUuUuUuUuU");
+    }
+
 }
