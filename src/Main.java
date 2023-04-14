bb15ab022070573b22cb3efcce9f3aa367323f2a
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-04-14 10:01:57 -0500

    Commit del metodo con nombre

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void sebastianDiaz(){
+        System.out.println("Hola, soy Sebastian Diaz");
+    }
 }
