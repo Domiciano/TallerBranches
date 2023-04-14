876e92b25203ad48ac679eb0ed13aec7c5fd4da5
Dantesiio <DavidDo29@hotmail.com>
2023-04-14 10:01:40 -0500

    amen

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        David_Donneys david=new David_Donneys();
+        david.jsahduhagiafb();
+        System.out.println("suma = "+suma());
         System.out.println("Hello world!");
+    }
+
+    public static int suma() {
+        int a=1;
+        int result=0;
+        result=a+a;
+        return result;
     }
 }
