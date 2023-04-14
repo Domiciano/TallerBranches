cd27ba43cc35869dbe1b9f8449ed6cefc70de12d
RafaelaRuizP <rafaela.ruiz@u.icesi.edu.co>
2023-04-14 10:02:06 -0500

    Hey

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    public void topDilfsDeMiCorazon(){
+        System.out.println("1. Mads Mikkelsen");
+        System.out.println("2. Robert Downey Jr");
+        System.out.println("3. Chris Evans");
+        System.out.println("4. Henry Cavill");
+        System.out.println("5. Oscar Isaac");
+
+    }
 }
